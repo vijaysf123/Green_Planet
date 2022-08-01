82e2e085ef88da6a1e4592043480f4346cd97ac8
vijaysf123 <109033340+vijaysf123@users.noreply.github.com>
2022-08-01 10:30:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 const menu = document.getElementById("menu");
-const navbar = document.getElementById("navbar")
+const navbar = document.getElementById("navbar");
 
 menu.addEventListener('click',function(){
     menu.classList.toggle("fa-times")
     navbar.classList.toggle("nav-toggle")
 });
+
 window.addEventListener("scroll", function(){
     navbar.classList.remove("nav-toggle")
     menu.classList.remove("fa-times")
